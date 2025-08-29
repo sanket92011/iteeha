d8be4503f0726c1cd829bc4ef97c27325f9c3dca
Sanket ambaliya <intern3.topai24@gmail.com>
2025-08-29 19:35:35 +0530

    resolved payment issue

--- lib/common_widgets/in_app_browser_screen.dart
+++ lib/common_widgets/in_app_browser_screen.dart
@@ -1,138 +1,10 @@
-// import 'dart:io';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:permission_handler/permission_handler.dart';
-
-// import 'package:webview_flutter/webview_flutter.dart';
-
-// import 'circular_loader.dart';
-// import 'custom_dialog.dart';
-
-// class InAppBrowserScreen extends StatefulWidget {
-//   const InAppBrowserScreen({super.key});
-
-//   @override
-//   State<InAppBrowserScreen> createState() => _InAppBrowserScreenState();
-// }
-
-// class _InAppBrowserScreenState extends State<InAppBrowserScreen> {
-//   bool isLoading = true;
-
-//   bool iOSCondition(double dH) => Platform.isIOS && dH > 850;
-
-//   WebViewController? controller;
-
-//   String link = 'https://order.iteeha.coffee/';
-//   setController() {
-//     // controller = WebViewController()
-//     //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
-//     //   ..setBackgroundColor(const Color(0x00000000))
-//     //   ..setNavigationDelegate(
-//     //     NavigationDelegate(
-//     //       onPageStarted: (String url) {
-//     //         if (isLoading) setState(() => isLoading = false);
-//     //       },
-//     //       onNavigationRequest: (NavigationRequest request) {
-//     //         return NavigationDecision.navigate;
-//     //       },
-//     //       onUrlChange: (change) {},
-//     //     ),
-//     //   )
-//     //   ..loadRequest(Uri.parse(link));
-//   }
-
-//   Future<bool> goBack() async {
-//     bool canGoBack = await controller!.canGoBack();
-//     if (canGoBack) {
-//       controller!.goBack();
-//     } else {
-//       closeApp();
-//     }
-//     return false;
-//   }
-
-//   closeApp() {
-//     return showDialog(
-//       context: context,
-//       builder: ((context) => CustomDialog(
-//             title: "Are you sure you want to exit the application?",
-//             noText: 'No',
-//             yesText: 'Yes',
-//             subTitle: '',
-//             noFunction: () {
-//               Navigator.of(context).pop();
-//             },
-//             yesFunction: () async {
-//               Navigator.of(context).pop();
-//               SystemNavigator.pop();
-//             },
-//           )),
-//     );
-//   }
-
-//   requestCameraPermission() async {
-//     var status = await Permission.camera.request();
-//     if (status.isDenied) {
-//     } else if (status.isPermanentlyDenied) {
-//       openAppSettings();
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     setController();
-//   }
-
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     if (controller != null) controller!.clearCache();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final dH = MediaQuery.of(context).size.height;
-//     final dW = MediaQuery.of(context).size.width;
-
-//     return WillPopScope(
-//       onWillPop: () => goBack(),
-//       child: Scaffold(
-//         body:
-//             // iOSCondition(dH) ? screenBody(dW) :
-//             SafeArea(child: screenBody(dW)),
-//       ),
-//     );
-//   }
-
-//   screenBody(double dW) {
-//     return Stack(
-//       children: [
-//         WebView(
-//           initialUrl: link,
-//           zoomEnabled: false,
-//           javascriptMode: JavascriptMode.unrestricted,
-//           onPageFinished: (String url) {
-//             setState(() => isLoading = false);
-//           },
-//           onWebViewCreated: (WebViewController webViewController) {
-//             controller = webViewController;
-//           },
-//         ),
-//         if (isLoading) const CircularLoader()
-//       ],
-//     );
-//   }
-// }
-
-// ignore_for_file: unused_local_variable
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 import '../authModule/providers/auth_provider.dart';
 import '../common_functions.dart';
 import 'circular_loader.dart';
@@ -158,7 +30,6 @@ class _InAppBrowserScreenState extends State<InAppBrowserScreen> {
       setState(() {
         link = response['result'][0]['value'];
       });
-      // Load the URL after getting it
       if (controller != null && link.isNotEmpty) {
         controller!.loadRequest(Uri.parse(link));
       }
@@ -181,26 +52,44 @@ class _InAppBrowserScreenState extends State<InAppBrowserScreen> {
     return showDialog(
       context: context,
       builder: ((context) => CustomDialog(
-            title: "Are you sure you want to exit the application?",
-            noText: 'No',
-            yesText: 'Yes',
-            subTitle: '',
-            noFunction: () {
-              Navigator.of(context).pop();
-            },
-            yesFunction: () async {
-              Navigator.of(context).pop();
-              SystemNavigator.pop();
-            },
-          )),
+        title: "Are you sure you want to exit the application?",
+        noText: 'No',
+        yesText: 'Yes',
+        subTitle: '',
+        noFunction: () {
+          Navigator.of(context).pop();
+        },
+        yesFunction: () async {
+          Navigator.of(context).pop();
+          SystemNavigator.pop();
+        },
+      )),
     );
   }
 
   requestCameraPermission() async {
     var status = await Permission.camera.request();
-    if (status.isDenied) {
-    } else if (status.isPermanentlyDenied) {
+    if (status.isPermanentlyDenied) {
       openAppSettings();
+    }
+  }
+
+  /// 🔑 Helper: Launch with specific package (if available)
+  Future<void> _launchWithPackage(String upiUrl, String packageName, String? fallbackUrl) async {
+    final uri = Uri.parse(upiUrl);
+
+    final launched = await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+      webViewConfiguration: const WebViewConfiguration(
+        headers: <String, String>{},
+      ),
+      webOnlyWindowName: packageName, // Helps Android pick specific package
+    );
+
+    if (!launched && fallbackUrl != null) {
+      // If app not installed, open fallback (Play Store)
+      await launchUrl(Uri.parse(fallbackUrl), mode: LaunchMode.externalApplication);
     }
   }
 
@@ -216,7 +105,55 @@ class _InAppBrowserScreenState extends State<InAppBrowserScreen> {
           onPageFinished: (String url) {
             setState(() => isLoading = false);
           },
-          onNavigationRequest: (NavigationRequest request) {
+          onNavigationRequest: (NavigationRequest request) async {
+            final url = request.url;
+
+            // Handle direct UPI links
+            if (url.startsWith("upi:")) {
+              final uri = Uri.parse(url);
+              if (await canLaunchUrl(uri)) {
+                await launchUrl(uri, mode: LaunchMode.externalApplication);
+              }
+              return NavigationDecision.prevent;
+            }
+
+            // Handle intent:// links
+            if (url.startsWith("intent://")) {
+              try {
+                // Extract package name
+                final packageRegex = RegExp(r'package=([\w\.]+);');
+                final match = packageRegex.firstMatch(url);
+                final packageName = match != null ? match.group(1) : null;
+
+                // Extract fallback
+                String? fallbackUrl;
+                if (url.contains("browser_fallback_url=")) {
+                  fallbackUrl = Uri.decodeComponent(
+                    url.split("browser_fallback_url=").last.split(";").first,
+                  );
+                }
+
+                // Convert intent:// to upi ://
+                final upiUrl = url.replaceFirst("intent://", "upi://");
+
+                if (packageName != null) {
+                  await _launchWithPackage(upiUrl, packageName, fallbackUrl);
+                } else {
+                  // No package given, just try normal UPI launch
+                  final uri = Uri.parse(upiUrl);
+                  if (await canLaunchUrl(uri)) {
+                    await launchUrl(uri, mode: LaunchMode.externalApplication);
+                  } else if (fallbackUrl != null) {
+                    await launchUrl(Uri.parse(fallbackUrl),
+                        mode: LaunchMode.externalApplication);
+                  }
+                }
+              } catch (e) {
+                debugPrint("Error handling intent:// $e");
+              }
+              return NavigationDecision.prevent;
+            }
+
             return NavigationDecision.navigate;
           },
         ),
@@ -233,32 +170,25 @@ class _InAppBrowserScreenState extends State<InAppBrowserScreen> {
   @override
   void dispose() {
     super.dispose();
-    if (controller != null) controller!.clearCache();
+    controller?.clearCache();
   }
 
   @override
   Widget build(BuildContext context) {
-    final dH = MediaQuery.of(context).size.height;
-    final dW = MediaQuery.of(context).size.width;
-
     return WillPopScope(
       onWillPop: () => goBack(),
       child: Scaffold(
         body: SafeArea(
-          child: isLoading ? const CircularLoader() : screenBody(dW),
+          child: Stack(
+            children: [
+              controller != null
+                  ? WebViewWidget(controller: controller!)
+                  : const Center(child: Text('Loading...')),
+              if (isLoading) const CircularLoader(),
+            ],
+          ),
         ),
       ),
-    );
-  }
-
-  screenBody(double dW) {
-    return Stack(
-      children: [
-        controller != null
-            ? WebViewWidget(controller: controller!)
-            : const Center(child: Text('Loading...')),
-        if (isLoading) const CircularLoader()
-      ],
     );
   }
 }
